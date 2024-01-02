2035d663a79e438fdd73c42fe11b6afc3fe214eb
developertamimtaz <farhanahmedtaz21@gmail.com>
2024-01-02 12:50:30 +0600

    array-split

---
+++ array-split.js
@@ -0,0 +1,12 @@
+//JavaScript array split.
+const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(2, 5);
+
+//JavaScript array splice.
+const removed = nums.splice(2, 3, 77);
+// console.log(removed);
+// console.log(nums);
+
+//JavaScript array join.
+const together = nums.join(" ami ")
+console.log(together);
